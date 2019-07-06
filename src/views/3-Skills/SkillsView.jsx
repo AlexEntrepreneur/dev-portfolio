c7544c9a70ee2bcb97bbe075ac4ff07f9fb433c1
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-07-06 03:35:26 +0100

    Add "Skills Stack" Content Structure

--- src/views/3-Skills/SkillsView.jsx
+++ src/views/3-Skills/SkillsView.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { View } from '../../components/~reusables/atoms/atoms';
+import { View, Container } from '../../components/~reusables/atoms/atoms';
+import { SkillCard } from '../../components/~reusables/molecules/SkillCard';
+import { small_space } from '../../components/~reusables/variables/spacing';
 
 function SkillsView() {
   return (
     <View hCenter>
       <h2>Skills Stack</h2>
+      <Container fullWidth column padding={small_space}>
+        <SkillCard hCenter>
+          UX
+        </SkillCard>
+        <SkillCard hCenter>
+          <div className="title">UI Design</div>
+          ...
+        </SkillCard>
+        <Container>
+          <SkillCard fullWidth hCenter>
+            <div className="title">Frontend Web</div>
+            ...
+          </SkillCard>
+          <SkillCard hCenter width="30%">
+            <div className="title">Tooling</div>
+            ...
+          </SkillCard>
+        </Container>
+        <Container>
+          <SkillCard fullWidth hCenter>
+            <div className="title">Backend</div>
+            ...
+          </SkillCard>
+          <SkillCard hCenter width="30%">
+            <div className="title">Tooling</div>
+            ...
+          </SkillCard>
+        </Container>
+        <SkillCard hCenter>
+          <div className="title">Data Storage</div>
+          ...
+        </SkillCard>
+        <SkillCard hCenter>
+          <div className="title">Computer Science</div>
+          ...
+        </SkillCard>
+      </Container>
       <Link to='/projects'>
         <button>↓</button>
       </Link>
