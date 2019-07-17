4042ff9f6afd0db8cc1a8ece3d5f43eea52db785
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-07-17 21:58:38 +0100

    Add Intro Card + Rearrange Intro View Content

--- src/views/1-Intro/IntroView.jsx
+++ src/views/1-Intro/IntroView.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { View, HeadingText, Text, Button } from '../../components/~reusables/atoms/atoms';
+import { View, HeadingText, Text, Button, Card, Link, SmallText, Container } from '../../components/~reusables/atoms/atoms';
 import { small_space } from '../../components/~reusables/variables/spacing';
 
 function IntroView() {
   return (
     <View vCenter hCenter>
-      <HeadingText marginBottom={small_space}>"Writing Good Code is a UX Problem."</HeadingText>
-      <Text>Hello! I’m Alex, a designer turned fullstack web developer.</Text>
-      <Link to='/about'>About Me</Link>
-      <Link to='/skills'>
-        <Button>↓</Button>
+      <Link to='/about' style={{textDecoration:'none'}}>
+        <Card column vCenter hCenter>
+          <HeadingText marginBottom={small_space} marginTop="0" hCenter>"Writing Good Code is a UX Problem."</HeadingText>
+          <Text hCenter noMargin>Hello! I’m Alex, a designer turned fullstack web developer.</Text>
+        </Card>
       </Link>
+      <SmallText>Tap to Find Out More</SmallText>
+
+      <Container column hCenter>
+        <SmallText noMargin>Skills</SmallText>
+        <Link to='/skills'>
+          <Button>↓</Button>
+        </Link>
+      </Container>
     </View>
   );
 }
